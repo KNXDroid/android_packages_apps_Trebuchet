LMODroid/platform_packages_apps_Launcher3

Launcher3: Add recents shortcut for floating window
using LMOFreeform.

Change-Id: Ia6da8c4a61a2c14e1f7750994bd25961e47f5e9d

quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -25,6 +25,7 @@ import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
 import android.app.IActivityManager;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -266,6 +267,42 @@ public interface TaskShortcutFactory {
         }
     }
 
+    class FloatingSystemShortcut extends SystemShortcut<BaseDraggingActivity> {
+        private static final Intent sFreeformIntent =
+                new Intent("com.libremobileos.freeform")
+                .setPackage("com.libremobileos.freeform.START_FREEFORM")
+
+        private final TaskView mTaskView;
+
+        public FloatingSystemShortcut(BaseDraggingActivity activity,
+                TaskIdAttributeContainer taskContainer) {
+            // TODO new icon?
+            super(R.drawable.float_portrait_2_24px, R.string.floating_window,
+                    activity, taskContainer.getItemInfo(), taskContainer.getTaskView());
+            mTaskView = taskContainer.getTaskView();
+        }
+
+        @Override
+        public void onClick(View view) {
+            dismissTaskMenuView(mTarget);
+            RecentsView rv = mTarget.getOverviewPanel();
+            rv.switchToScreenshot(() -> {
+                rv.finishRecentsAnimation(true /* toRecents */, false /* shouldPip */, () -> {
+                    mTarget.returnToHomescreen();
+                    rv.getHandler().post(this::startLmoFreeform);
+                });
+            });
+        }
+
+        private void startLmoFreeform() {
+            final Intent intent = sFreeformIntent
+                    .putExtra("packageName", mTaskView.getTask().key.getPackageName())
+                    .putExtra("activityName", mTaskView.getTask().getTopComponent().getClassName())
+                    .putExtra("userId", mTaskView.getTask().key.userId);
+            mTarget.getApplicationContext().sendBroadcast(intent);
+        }
+    }
+
     /**
      * Does NOT add split options in the following scenarios:
      * * The taskView to add split options is already showing split screen tasks
@@ -365,6 +402,19 @@ public interface TaskShortcutFactory {
             return ActivityManagerWrapper.getInstance().supportsFreeformMultiWindow(activity)
                     && !SystemProperties.getBoolean("persist.wm.debug.desktop_mode", false)
                     && !SystemProperties.getBoolean("persist.wm.debug.desktop_mode_2", false);
+        }
+    };
+
+    TaskShortcutFactory FLOATING = new TaskShortcutFactory() {
+        @Override
+        public List<SystemShortcut> getShortcuts(BaseDraggingActivity activity,
+                TaskIdAttributeContainer taskContainer) {
+            final Task task  = taskContainer.getTask();
+            if (!task.isDockable) {
+                return null;
+            }
+
+            return Collections.singletonList(new FloatingSystemShortcut(activity, taskContainer));
         }
     };
 

KNXDroid/android_packages_apps_Trebuchet

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -25,6 +25,9 @@ import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCH
 import static com.android.launcher3.util.SplitConfigurationOptions.STAGE_POSITION_BOTTOM_OR_RIGHT;
 
 import android.app.ActivityOptions;
+import android.app.IActivityManager;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -289,6 +292,43 @@ public interface TaskShortcutFactory {
         }
     }
 
+    class FloatingSystemShortcut extends SystemShortcut<RecentsViewContainer> {
+        private static final String FREEFORM_PACKAGE = "com.libremobileos.freeform";
+        private static final String FREEFORM_INTENT = "com.libremobileos.freeform.START_FREEFORM";
+
+        private final TaskView mTaskView;
+
+        public FloatingSystemShortcut(RecentsViewContainer container, TaskContainer taskContainer) {
+            // TODO new icon?
+            super(R.drawable.float_portrait_2_24px, R.string.floating_window,
+                    container, taskContainer.getItemInfo(), taskContainer.getTaskView());
+            mTaskView = taskContainer.getTaskView();
+        }
+
+        @Override
+        public void onClick(View view) {
+            dismissTaskMenuView();
+            RecentsView rv = mTarget.getOverviewPanel();
+            rv.switchToScreenshot(() -> {
+                rv.finishRecentsAnimation(true /* toRecents */, false /* shouldPip */, () -> {
+                    mTarget.returnToHomescreen();
+                    rv.getHandler().post(this::startLmoFreeform);
+                });
+            });
+        }
+
+        private void startLmoFreeform() {
+            final Task task = mTaskView.getFirstTask();
+            final Intent intent = new Intent(FREEFORM_INTENT)
+                    .setPackage(FREEFORM_PACKAGE)
+                    .putExtra("packageName", task.key.getPackageName())
+                    .putExtra("activityName", task.getTopComponent().getClassName())
+                    .putExtra("userId", task.key.userId)
+                    .putExtra("taskId", task.key.id);
+            mTarget.asContext().sendBroadcast(intent);
+        }
+    }
+
     /**
      * Does NOT add split options in the following scenarios:
      * * 1. Taskbar is not present AND aren't at least 2 tasks in overview to show split options for
@@ -395,6 +435,19 @@ public interface TaskShortcutFactory {
                     container.asContext().getContentResolver(),
                     Settings.Global.DEVELOPMENT_ENABLE_FREEFORM_WINDOWS_SUPPORT, 0) != 0
                     && !DesktopModeStatus.canEnterDesktopMode(container.asContext());
+        }
+    };
+
+    TaskShortcutFactory FLOATING = new TaskShortcutFactory() {
+        @Override
+        public List<SystemShortcut> getShortcuts(RecentsViewContainer container,
+                TaskContainer taskContainer) {
+            final Task task  = taskContainer.getTask();
+            if (!task.isDockable) {
+                return null;
+            }
+
+            return Collections.singletonList(new FloatingSystemShortcut(container, taskContainer));
         }
     };
 
c9bae0ea70c11def111650c14beffcab3c09be7d
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-30 09:46:19 +0800

    Launcher3: clean heap memory when leaving launcher
    Change-Id: I3271e544f37ef2242366681d134dd005284f16f8

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -331,6 +331,9 @@ public class Launcher extends StatefulActivity<LauncherState>
     private final KeyboardShortcutsDelegate mKeyboardShortcutsDelegate =
             new KeyboardShortcutsDelegate(this);
 
+    private static final long GC_INTERVAL_MS = 10 * 60 * 1000L; // 10 minutes
+    private long lastGcTime = 0L;
+
     @Thunk
     Workspace<?> mWorkspace;
     @Thunk
@@ -1286,6 +1289,20 @@ public class Launcher extends StatefulActivity<LauncherState>
         return Optional.of(LAUNCHER_ALLAPPS_EXIT);
     }
 
+    private final Runnable mLauncherGcOpt = new Runnable() {
+        @Override
+        public void run() {
+            long currentTime = System.currentTimeMillis();
+            if (lastGcTime == 0L || currentTime - lastGcTime > GC_INTERVAL_MS) {
+                Log.v("GcOpt", "performing garbage collection for Launcher");
+                System.gc();
+                System.runFinalization();
+                System.gc();
+                lastGcTime = currentTime;
+            }
+        }
+    };
+
     @Override
     protected void onResume() {
         TraceHelper.INSTANCE.beginSection(ON_RESUME_EVT);
@@ -1296,6 +1313,8 @@ public class Launcher extends StatefulActivity<LauncherState>
         } else {
             mOverlayManager.onActivityResumed();
         }
+        
+        mHandler.removeCallbacks(mLauncherGcOpt);
 
         DragView.removeAllViews(this);
         TraceHelper.INSTANCE.endSection();
@@ -1315,6 +1334,8 @@ public class Launcher extends StatefulActivity<LauncherState>
             mOverlayManager.onActivityPaused();
         }
         mAppWidgetHolder.setActivityResumed(false);
+        
+        mHandler.postDelayed(mLauncherGcOpt, 1000);
     }
 
     /**

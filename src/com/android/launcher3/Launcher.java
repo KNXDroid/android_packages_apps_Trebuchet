5cf148fadfa54a916ed491815767041d4ba39e13
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-01 08:32:09 +0800

    Launcher3: release memory whenever possible
    Change-Id: I0e7d556507fb5c8ccb4031a1e96f3154695d074f
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1318,6 +1318,15 @@ public class Launcher extends StatefulActivity<LauncherState>
 
         DragView.removeAllViews(this);
         TraceHelper.INSTANCE.endSection();
+        
+        releaseMemory();
+    }
+
+    private void releaseMemory() {
+        try {
+            android.app.ActivityManager.getService().releaseMemory(900, 20, false, false);
+        } catch (Exception e) {
+        }
     }
 
     @Override
@@ -1336,6 +1345,8 @@ public class Launcher extends StatefulActivity<LauncherState>
         mAppWidgetHolder.setActivityResumed(false);
         
         mHandler.postDelayed(mLauncherGcOpt, 1000);
+        
+        releaseMemory();
     }
 
     /**
